d9c8041840852bfea380badfe32b22fa7c64d7dd
Nick Allen <rutherford.t.b@gmail.com>
2022-01-02 17:27:35 -0600

    remove old public folder

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('server/public'));
+// app.use(express.static('server/public'));
 
 // Routes
 app.use('/tasks', tasksRouter);
