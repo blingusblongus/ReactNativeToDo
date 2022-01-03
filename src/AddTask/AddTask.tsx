ec5f517dd773d96bae5dd7e351502f9bc67193eb
Nick Allen <rutherford.t.b@gmail.com>
2022-01-02 21:01:13 -0600

    addtask stylesheet started

--- src/AddTask/AddTask.tsx
+++ src/AddTask/AddTask.tsx
@@ -2,35 +2,62 @@ import React, { useState } from 'react';
 import DatePicker from 'react-native-date-picker';
 import {
     View,
-    TextInput
+    TextInput,
+    Text,
+    StyleSheet,
 } from 'react-native';
 
 import axios from 'axios';
 
 function AddTask(props) {
     const [date, setDate] = useState(new Date())
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
+    const [task, setTask] = useState({
+        description: '',
+        dateDue: date,
+        complete: false
+    });
 
-    
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+        },
+        label:{
+            flex: 1,
+            color: 'black',
+        },
+        input:{
+            flex: 1,
+            borderWidth: 1,
+        }
+    })
+
+    console.log(task);
+
     return (
         <View>
-            <TextInput>Description:</TextInput>
-            <DatePicker 
-            modal
-            date={date} 
-            open={open}
-            onConfirm={(date)=> {
-                setOpen(false);
-                setDate(date);
-            }}
-            onCancel={()=>{
-                setOpen(false)
-            }}
+            <View style={styles.container}>
+                <Text style={styles.label}>Description:</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={(text) => setTask({ ...task, description: text })}
+                ></TextInput>
+            </View>
+            <Text style={styles.label}>Date Due:  {date.toISOString().split('T')} </Text>
+            <DatePicker
+                modal
+                date={date}
+                open={open}
+                onConfirm={(date) => {
+                    setOpen(false);
+                    setDate(date);
+                }}
+                onCancel={() => {
+                    setOpen(false)
+                }}
             ></DatePicker>
         </View>
     )
-
-    console.log(date);
 }
 
 export default AddTask;
