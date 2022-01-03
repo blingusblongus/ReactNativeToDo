1930bb967d1925a59f9bda76b0fc9d99246ba4a0
Nick Allen <rutherford.t.b@gmail.com>
2022-01-02 20:33:00 -0600

    add modal mode to date-picker

--- src/AddTask/AddTask.tsx
+++ src/AddTask/AddTask.tsx
@@ -2,15 +2,35 @@ import React, { useState } from 'react';
 import DatePicker from 'react-native-date-picker';
 import {
     View,
+    TextInput
 } from 'react-native';
+
+import axios from 'axios';
 
 function AddTask(props) {
     const [date, setDate] = useState(new Date())
+    const [open, setOpen] = useState(true);
+
+    
     return (
         <View>
-            <DatePicker date={date}></DatePicker>
+            <TextInput>Description:</TextInput>
+            <DatePicker 
+            modal
+            date={date} 
+            open={open}
+            onConfirm={(date)=> {
+                setOpen(false);
+                setDate(date);
+            }}
+            onCancel={()=>{
+                setOpen(false)
+            }}
+            ></DatePicker>
         </View>
     )
+
+    console.log(date);
 }
 
 export default AddTask;
